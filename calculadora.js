267e0fa044598940e1c681e19f724fa10ae4b156
Santiago Prada <sanprabet@gmail.com>
2020-06-18 20:41:52 -0500

    Agregamos los botones y la pantalla al JS

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorNumber = document.querySelectorAll('[data-operator]')
+const clearButton = document.querySelector('[data-all-clear]')
+const deleteButton = document.querySelector('[data-delete]')
+const equalButton = document.querySelector('[data-equals]')
+
